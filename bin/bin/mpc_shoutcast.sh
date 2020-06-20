b194400a3d97c031628344e6567458e0291527ae
fagcinsk <fagci.nsk@gmail.com>
2020-06-20 23:57:10 +0700

    shoutcast script improvements

---
+++ bin/bin/mpc_shoutcast.sh
@@ -0,0 +1,189 @@
+#!/usr/bin/env bash
+
+# ===================================================
+#
+# Shoutcast mpc radio player
+#
+# Author: Mikhail Yudin aka fagci
+# Description: uses jq json parser and mpc to tunein
+#
+# ===================================================
+
+set -o nounset
+set -o errexit
+
+readonly CACHE_TIME=600
+readonly BASE_URL='http://api.shoutcast.com'
+
+readonly CONFIG_FILE="${HOME}/.config/shoutcast.config"
+readonly SCRIPT_NAME=$(basename $0)
+readonly ARGS="$@"
+
+is_config_file_absent() {
+    [[ ! -f "${CONFIG_FILE}" ]]
+}
+
+create_new_config() {
+    cat > "${CONFIG_FILE}" << EOL
+SHOUTCAST_API_KEY=YOUR_DEV_API_TOKEN
+SEARCH_LIMIT=20
+EOL
+}
+
+load_config() {
+    source "${CONFIG_FILE}"
+}
+
+check_config_and_load() {
+    is_config_file_absent \
+        && create_new_config \
+        && edit_config
+
+    load_config
+
+    if [[ -z $SHOUTCAST_API_KEY ]]; then
+        rm "${CONFIG_FILE}" 
+        exec $(basename $0)
+    fi
+}
+
+edit_config() {
+    $EDITOR "${CONFIG_FILE}"
+}
+
+make_query() {
+    local method=$1
+    local params=$2
+
+    curl -s -G \
+        --data-urlencode "k=${SHOUTCAST_API_KEY}" \
+        --data-urlencode "f=json" \
+        --data-urlencode "limit=${SEARCH_LIMIT:-20}" \
+        --data-urlencode "$params" \
+        "${BASE_URL}/${method}" 
+    }
+
+tunein() {
+    local base=$1
+    local station_id=$2
+
+    mpc -q clear
+    mpc -q -w load "http://yp.shoutcast.com/${base}?id=${station_id}"
+    mpc -q play
+}
+
+get_hash() {
+    md5sum <<< "$1" | cut -d ' ' -f 1
+}
+
+get_cache_file_name() {
+    local hash=$(get_hash $1)
+    echo "${TMPDIR:-/tmp}/${SCRIPT_NAME}.$hash.json"
+}
+
+cache_get() {
+    local tmpfile=$(get_cache_file_name $1)
+    if [[ -f "${tmpfile}" ]]; then
+        lifetime=$(expr $(date +%s) - $(date -r "${tmpfile}" +%s))
+        [ $lifetime -gt $CACHE_TIME ] && rm "${tmpfile}"
+    fi
+    [[ -f "${tmpfile}" ]] && cat "${tmpfile}"
+}
+
+cache_set() {
+    local tmpfile=$(get_cache_file_name $1)
+    echo "$2" > "${tmpfile}"
+}
+
+make_search_query() {
+    local query="$1"
+    local result=$(cache_get $query)
+
+    if [[ -z "$result" ]]; then
+        result=$(make_query 'station/advancedsearch' "search=${query}")
+        cache_set "$query" "$result"
+    fi
+
+    echo "$result"
+}
+
+search() {
+    local query="$1"
+    local items_path='to_entries[]|(.key|tostring)+"\t"+(.value.br|tostring)+"\t"+.value.name'
+
+
+    clear
+
+    echo 'Searching...'
+
+    local json=$(make_search_query "$query")
+
+    local stationlist_json=$(jq -cM '.response.data.stationlist' <<< "$json")
+    local stations_json=$(jq -cM '.station | if type == "object" then [.] else . end' <<< "$stationlist_json")
+    local stations_list=$(jq -crM "$items_path" <<< "$stations_json")
+
+
+    clear
+
+    echo -e "Found for '${query}':\n" 
+    echo -e "Num\tBR\tName\n"
+    echo "${stations_list/&amp;/&}"
+
+    echo ''
+    read -p 'Tunein to: ' num
+
+    local base=$(jq -r '.tunein["base-m3u"]' <<< "$stationlist_json")
+    local station_id=$(jq -r ".[${num}].id" <<< "$stations_json")
+    local station_name=$(jq -r ".[${num}].name" <<< "$stations_json")
+
+    echo "Tuning to '${station_name}'..."
+
+    tunein $base $station_id
+}
+
+ask_for_search() {
+    local query="${1:-}"
+
+    clear
+
+    [[ -z $query ]] \
+        && read -p 'Search: ' query
+
+    search "$query"
+}
+
+
+show_help() {
+    cat <<-END
+Shoutcast mpc radio player
+
+Usage: ${SCRIPT_NAME} [args] [search_query]
+
+Args:
+
+    -c, --config    edit config file
+    -h, --help      show help
+END
+    exit 0
+}
+
+cmdline() {
+    [ $# -eq 0 ] && ask_for_search
+
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            --config*|-c*) edit_config ;;
+            --help|-h) show_help ;;
+            *) ask_for_search "$1" ;;
+        esac
+        shift
+    done
+}
+
+main() {
+    check_config_and_load
+    cmdline $ARGS
+}
+
+main
+
