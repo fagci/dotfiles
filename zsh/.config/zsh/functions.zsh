e189d1ea965c1baac004707efc44e8e3de3d4b47
Mikhail Yudin <fagci.nsk@gmail.com>
2021-02-25 13:15:38 +0700

    func

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -22,25 +22,7 @@ geoip() {
 fcd() {
   cd $(find -type d -print 2> /dev/null | fzf)
 }
-# functions
 
-share() {
-  curl --progress-bar -F"file=@$1" https://0x0.st | tee /dev/null
-}
-
-wttr() {
-  wget -qO- wttr.in/\?nqM
-}
-
-cht() {
-  where="$1"; shift
-  IFS=+ curl "https://cht.sh/$where/$*"
-}
-
-geoip() {
-  curl https://ipapi.co/${1}/json/
-}
-
-fcd() {
-  cd $(find -type d -print 2> /dev/null | fzf)
+extip() {
+    curl https://ifconfig.me
 }
