38c5cf3f49879ea9ef48a220a207290d748cea28
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-11 10:22:39 +0700

    autoindent for php, better matchup hl

--- nvim/.config/nvim/lua/styles.lua
+++ nvim/.config/nvim/lua/styles.lua
@@ -3,6 +3,6 @@ vim.cmd([[colorscheme gruvbox]])
 vim.cmd([[
 augroup matchup_matchparen_highlight
   autocmd!
-  autocmd ColorScheme * hi MatchParen guibg=#444444
+  autocmd ColorScheme * hi MatchParen guibg=#333333
 augroup END
 ]])
