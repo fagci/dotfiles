b418688554d37236b087b4ffc616d20701d2b001
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-12 00:58:04 +0700

    ts inc sel

--- nvim/.config/nvim/plugin/treesitter.lua
+++ nvim/.config/nvim/plugin/treesitter.lua
@@ -3,6 +3,15 @@ ts_configs.setup {
   ensure_installed = 'maintained',
   highlight = { enable = true, use_languagetree = true },
   indent = { enable = false },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
   refactor = {
     smart_rename = { enable = true, keymaps = { smart_rename = ',r' } },
     highlight_definitions = { enable = true },
