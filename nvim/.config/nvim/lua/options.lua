429f7ef641b5964a4dcee84e567c0d8fcd832979
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-13 17:46:15 +0700

    refactor(format options): single string

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -61,16 +61,6 @@ opt.inccommand = 'nosplit'
 
 opt.shortmess:append "c"
 
-opt.formatoptions = opt.formatoptions
-  - "a" -- Auto formatting is BAD.
-  - "t" -- Don't auto format my code. I got linters for that.
-  + "c" -- In general, I like it when comments respect textwidth
-  + "q" -- Allow formatting comments w/ gq
-  - "o" -- O and o, don't continue comments
-  + "r" -- But do continue when pressing enter.
-  + "n" -- Indent past the formatlistpat, not underneath it.
-  + "j" -- Auto-remove comments if possible.
-  - "2" -- I'm not in gradeschool anymore
-
+opt.formatoptions = 'croqn'
 vim.cmd('filetype plugin indent on')
 vim.g.matchup_matchparen_offscreen = {method = 'popup'}
