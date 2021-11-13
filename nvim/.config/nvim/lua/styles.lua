a1326e8bff15ec600e701c0f71723e56fe94a332
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-13 15:10:01 +0700

    fix: disable italics in termux

--- nvim/.config/nvim/lua/styles.lua
+++ nvim/.config/nvim/lua/styles.lua
@@ -1,8 +1,17 @@
 vim.o.background = "dark"
+
+if string.find(vim.fn.getenv("PREFIX"), 'termux') then
+	vim.cmd([[
+	let g:gruvbox_italic=0
+	let g:gruvbox_italicize_comments=0
+	let g:gruvbox_italicize_strings=0
+	]])
+end
+
 vim.cmd([[colorscheme gruvbox]])
 vim.cmd([[
 augroup matchup_matchparen_highlight
-  autocmd!
-  autocmd ColorScheme * hi MatchParen guibg=#333333
+autocmd!
+autocmd ColorScheme * hi MatchParen guibg=#333333
 augroup END
 ]])
