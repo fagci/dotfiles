7318a9f00e10c175e71477c7ea8bbb2dfc32a7a9
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-27 20:28:15 +0700

    fix(zsh): prompt

--- zsh/.config/zsh/prompt/prompt.zsh
+++ zsh/.config/zsh/prompt/prompt.zsh
@@ -1,14 +1,13 @@
-setopt prompt_subst
-zstyle ':vcs_info:git:*' formats ' %b'
-
 autoload -Uz vcs_info colors
 colors
 
-precmd() {
-    vcs_info 
-    if [ -z "${SSH_CONNECTION}" ]; then
-        PROMPT="%(?..[%?] )%{${fg[yellow]}%}%~%{${reset_color}%}${vcs_info_msg_0_}> "
-    else
-        PROMPT="%(?..[%?] )%{${fg[green]}%}%n@%m %{${fg[yellow]}%}%~%{${reset_color}%}${vcs_info_msg_0_}> "
-    fi
-}
+precmd() { vcs_info }
+
+zstyle ':vcs_info:git:*' formats ' %b'
+setopt PROMPT_SUBST
+
+if [ -z "${SSH_CONNECTION}" ]; then
+    PROMPT='%(?..[%?] )%{${fg[yellow]}%}%~%{${reset_color}%}${vcs_info_msg_0_}> '
+else
+    PROMPT='%(?..[%?] )%{${fg[green]}%}%n@%m %{${fg[yellow]}%}%~%{${reset_color}%}${vcs_info_msg_0_}> '
+fi
