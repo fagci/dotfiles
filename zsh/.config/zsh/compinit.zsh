78a417c38e32f7a437f67a19bc1d4fa0b78d11f8
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-29 23:47:25 +0700

    feature(zsh): globdot

--- zsh/.config/zsh/compinit.zsh
+++ zsh/.config/zsh/compinit.zsh
@@ -3,7 +3,10 @@ zstyle ':completion:*' group-name ''
 zstyle ':completion:*' format '%F{yellow}%d:%f'
 zstyle ':completion::complete:*' use-cache on
 zstyle ':completion::complete:*' cache-path ~/.cache/zsh
-zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' list-suffixes
+zstyle ':completion:*' expand prefix suffix
 
 zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
 
