af4c10af53bb15a53b40169f1e4957f5aea9dd05
Mikhail Yudin <fagci.nsk@gmail.com>
2021-11-30 01:44:14 +0700

    fix(zsh): better completion

--- zsh/.config/zsh/compinit.zsh
+++ zsh/.config/zsh/compinit.zsh
@@ -1,6 +1,23 @@
 zstyle ':completion:*' menu select
+
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
 zstyle ':completion:*' group-name ''
-zstyle ':completion:*' format '%F{yellow}%d:%f'
+zstyle ':completion:*' verbose yes
+
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3>7?7:($#PREFIX+$#SUFFIX)/3))numeric)'
+
 zstyle ':completion::complete:*' use-cache on
 zstyle ':completion::complete:*' cache-path ~/.cache/zsh
 
@@ -17,5 +34,3 @@ for dump in ~/.zcompdump(N.mh+24); do
 done
 
 compinit -C
-
-_comp_options+=(globdots)
