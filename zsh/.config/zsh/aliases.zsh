85e4da272e61ba3eab612485573e3a8765fb30cd
Mikhail Yudin <fagci.nsk@gmail.com>
2021-12-14 00:54:16 +0700

    refactor: rm bad habits & redundants

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -6,33 +6,13 @@ alias gcam="git commit -am"
 alias gp="git push"
 alias gl="git pull"
 
+alias ls='ls --color=auto'
+alias l='ls'
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+
 alias cdd='cd $(dirs -lp | fzf +m)'
-
-if hash nvim 2> /dev/null; then
-    alias vim='nvim'
-fi
-
-if hash exa 2> /dev/null; then
-  alias ls='exa'
-  alias l='exa'
-  alias ll='exa -l'
-  alias la='exa -a'
-  alias lla='exa -la'
-  alias L='exa -G'
-  alias LL='exa -lG'
-  alias LA='exa -aG'
-  alias LLA='exa -laG'
-else
-  alias ls='ls --color=auto'
-  alias l='ls'
-  alias ll='ls -l'
-  alias la='ls -a'
-  alias lla='ls -la'
-  alias L='ls -G'
-  alias LL='ls -lG'
-  alias LA='ls -aG'
-  alias LLA='ls -laG'
-fi
 
 if [[ "$OSTYPE" -eq "linux-android" ]] && hash pkg 2> /dev/null; then
   alias pki="pkg install"
