a555b5978c790032dbe0f6b179456c4792b27ee6
Mikhail Yudin <fagci.nsk@gmail.com>
2022-03-01 10:46:02 +0700

    fix paris error

--- nvim/.config/nvim/lua/config/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/config/nvim-autopairs.lua
@@ -1,7 +1,3 @@
 require('nvim-autopairs').setup({
 	check_ts = true,
 })
--- insert `(` after select function or method item
-local cmp = require'cmp'
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
