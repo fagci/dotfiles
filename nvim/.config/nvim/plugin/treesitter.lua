75b9b70824a7af4570552c4f95d09d5d81fdf9a6
Mikhail Yudin <fagci.nsk@gmail.com>
2022-04-12 11:23:06 +0700

    fix: deprecation warn

--- nvim/.config/nvim/plugin/treesitter.lua
+++ nvim/.config/nvim/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 local ts_configs = require 'nvim-treesitter.configs'
 ts_configs.setup {
-  ensure_installed = 'maintained',
+  ensure_installed = 'all',
   highlight = {
       additional_vim_regex_highlighting = true,
       enable = true,
