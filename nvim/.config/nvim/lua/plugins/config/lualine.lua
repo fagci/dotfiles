7dc058c30482c0c36ff4440271e91e0c55b8e763
Mikhail Yudin <fagci.nsk@gmail.com>
2022-05-22 12:58:29 +0700

    lualine tune

--- nvim/.config/nvim/lua/plugins/config/lualine.lua
+++ nvim/.config/nvim/lua/plugins/config/lualine.lua
@@ -1,13 +1,33 @@
 require('lualine').setup {
     options = {
         theme = 'OceanicNext',
+        icons_enabled = false,
+        component_separators = {
+            left = "|",
+            right = "|"
+        },
+        section_separators = {
+            left = "",
+            right = ""
+        }
     },
     sections = {
         lualine_a = {},
-        lualine_b = { 'diagnostics', 'filename' },
-        lualine_c = { 'lsp_progress' },
-        lualine_x = { 'encoding', 'filetype' },
-        lualine_y = { 'location' },
+        lualine_b = {
+            {
+                "diagnostics",
+                symbols = { error = "E", warn = "W", hint = "H", info = "I" },
+            },
+            {
+                "filename",
+                symbols = { modified = "*", readonly = "(ro)" },
+            },
+        },
+        lualine_c = {
+            { 'lsp_progress', spinner_symbols = { '|', '/', '-', '\\' } }
+        },
+        lualine_x = { 'filetype' },
+        lualine_y = { '%l/%L' },
         lualine_z = {}
     },
     inactive_sections = {
