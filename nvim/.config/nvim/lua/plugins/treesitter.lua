947bd02eaa1947c4ca8123398a235e336fa15286
Mikhail Yudin <fagci.nsk@gmail.com>
2023-04-26 03:13:05 +0700

    fix: huge traffic usage on lame scrolling w/ holding arrows

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,13 +5,11 @@ return {
         event = { "BufReadPost", "BufNewFile" },
         config = function()
             require 'plugins.config.treesitter'
-            require("treesitter-context").setup()
             require('plugins.config.nvim-autopairs')
         end,
         dependencies = {
             'nvim-treesitter/nvim-treesitter-refactor',
             'nvim-treesitter/nvim-treesitter-textobjects',
-            'nvim-treesitter/nvim-treesitter-context',
             'windwp/nvim-autopairs',
             'windwp/nvim-ts-autotag',
         },
